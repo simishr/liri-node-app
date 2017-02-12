167930b6db554a121fdc91899ef40ebe3f3f1b79
simishr <smriti.shrestha271@gmail.com>
2017-02-11 20:07:11 -0500

    Final commit

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var Twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
-
+var inquirer = require("inquirer");
 
 // grabbing twitter auth keys from keys.js
 var keys = require("./keys.js");
@@ -30,19 +30,15 @@ function myTweets(){
 	client.get( twitterURL, function(error, tweet, response) {
 	  if(!error) {
 	  	for(let i = 0; i < tweet.length; i++) {
-	  		console.log("Tweet: " + tweet[i].text);
+	  		console.log((i+1) + " . Tweet: " + tweet[i].text);
 	  		console.log("Tweeted At: " + tweet[i].created_at);
 	  		} 
-		};
+		}
 	});
 }
 
 // function that pulls info about a song from spotify..
-function spotifyThisSong(){
-
-	var songName = process.argv[3];
-
-	if(songName){
+function spotifyThisSong(songName){
 
 		spotify.search({ type: 'track', query: songName }, function(err, data) {
 
@@ -50,39 +46,22 @@ function spotifyThisSong(){
 		        console.log('Error occurred: ' + err);
 		        return;
 
-		    } 
+		    } else {
 
-		 	function displayInfo() {
 				console.log("Artists: " + data.tracks.items[0].artists[0].name);
 			 	console.log("Song: " + data.tracks.items[0].name);
 			 	console.log("Preview: " + data.tracks.items[0].preview_url);
 				console.log("Album: " + data.tracks.items[0].album.name);
 			}
-
-		 	displayInfo();
 	    
 		});
 
-	} else {
-
-		spotify.search({ type: 'track', query: 'the sign' }, function(err, data) {
-
-		    if (err) {
-		        console.log('Error occurred: ' + err);
-		        return;
-		    }
-
-		 	displayInfo();
-		
-			});
-		};
 }
 
 // function that pulls info about movies from IMDB..
-function movieThis(){
+function movieThis(movieName){
 
-	var movieName = process.argv[3];
-	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&tomatoes=true&y=&plot=short&r=json";
 
 	request(queryUrl, function(error, response, body) {
  	
@@ -94,64 +73,80 @@ function movieThis(){
     console.log("Language: " + JSON.parse(body).Language);
     console.log("Plot: " + JSON.parse(body).Plot);
     console.log("Actors: " + JSON.parse(body).Actors);
-    // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Language);
-    // console.log("Rotten Tomatoes Url: " + JSON.parse(body).Language);
-
-
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoUserRating);
+    console.log("Rotten Tomatoes Url: " + JSON.parse(body).tomatoURL);
   
-});
-	//request('http://www.google.com', function (error, response, body) {
-  	//if (!error && response.statusCode == 200) {
-    
-  	//	}
-	//})
-}
-
-fs.appendFile("log.txt", "\n" + process.argv.slice(2), function(err){
-
-	if(err) throw err;
-
-	
-});
-
-switch (commands) {
-
-	case "my-tweets" : {
-		myTweets();
-		break;
-	}
-
-	case "spotify-this-song" : {
-		spotifyThisSong();
-		break;
-	}
-
-	case "movie-this" : {
-		movieThis();
-		break;
-	}
-
-	case "do-what-it-says" : {
-
-		fs.readFile("random.txt", "utf8", function(error, data){
-
-			if(error) throw error;
-			
-			var data = data.split(",");
-
-			if(data[0] === "my-tweets"){
-				myTweets();	
-
-			} else if(data[0] === "spotify-this-song"){
-				spotifyThisSong(data[1]);
-				
-			} else if(data[0] === "movie-this") {
-				movieThis(data[1]);
-			}
-				
 	});
-		break;
-	}
-
 }
 
+inquirer.prompt([{
+	type: "checkbox",
+	message: "Welcome to Liri! How can I assist you today?",
+	choices: ["Check recent tweets?", "Spotify a song?", "Curious about a movie?", "Can't decide!"],
+	name: "commands" 
+
+	}]).then (function(user){
+
+		fs.appendFile("log.txt", "\n" + user.commands[0], function(err){
+			if (err) throw err;
+		});
+
+		switch (user.commands[0]) {
+
+			case "Check recent tweets?": {
+				myTweets();
+				break;
+			}
+
+			case "Spotify a song?": {
+				inquirer.prompt([{
+					type: "input",
+					message: "Name a song: ",
+					name: "song",
+					default: "The Sign by ace of base"
+					}]).then(function(songName){
+						var songName = songName.song;
+						spotifyThisSong(songName);
+					});
+
+				break;
+			}
+
+			case "Curious about a movie?": {
+				inquirer.prompt([{
+					type: "input",
+					message: "Name a movie: ",
+					name: "movie",
+					default: "Mr.Nobody"
+					}]).then(function(movieName){
+						var movieName = movieName.movie;
+						movieThis(movieName);
+					});
+				
+				break;
+			}
+
+			case "Can't decide!": {
+
+				fs.readFile("random.txt", "utf8", function(error, data){
+
+					if(error) throw error;
+					
+					var data = data.split(",");
+					
+					if(data[0] === "Check recent tweets?") {
+						myTweets();	
+
+					} else if(data[0] === "Spotify a song?") {
+						spotifyThisSong(data[1]);
+						
+					} else if(data[0] === "Curious about a movie?") {
+						movieThis(data[1]);
+					}
+						
+			})
+				break;
+			}
+		}
+});																																																																		
+	
